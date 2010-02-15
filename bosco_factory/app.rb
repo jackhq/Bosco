977ce480d6301f420177da128b3f1a44de98e33c
Tom Wilson <tom@jackhq.com>
2010-02-15 11:47:44 -0500

    bosco_factory added

---
+++ bosco_factory/app.rb
@@ -0,0 +1,51 @@
+require 'sinatra'
+require 'haml'
+require 'bosco'
+require 'crack'
+require 'json'
+
+class App < Sinatra::Default
+
+  enable :sessions
+  
+  get '/' do
+    session[:forms] = [] unless session[:forms]
+    haml :index
+  end
+  
+  get '/forms/new' do
+    haml :new_bosco_form
+  end
+  
+  post '/forms' do
+    session[:forms] << { :name => params[:form][:name]}.to_json
+    redirect '/'
+  end
+  
+  get '/forms/:name' do |name|
+    
+  end
+  
+  
+  
+  
+  get '/questions/new' do
+    template = Crack::JSON.parse(open('question.json').read)
+    haml @engine = Bosco::Engine.new(
+      :form => template["form"],
+      :output => template["output"],
+      :css => template["css"] || '',
+      :javascript => template["javascript"] || ''
+    ).build_form
+  end
+  
+  post '/questions' do
+    
+    session[:form] << params[:question].to_json
+    haml :index
+    
+  end
+  
+  
+
+end
---
+++ bosco_factory/readme.md
@@ -0,0 +1,38 @@
+# SixHour
+
+Simple Template to a Web Applcation
+
+## Contents
+
+* [960 Grid](http://960.gs)
+* [JQuery](http://jquery.com)
+* [Haml](http://haml-lang.com/)
+* [RSpec](http://rspec.org)
+
+## Install
+
+    gem install sixhour
+    
+    gem unpack sixhour 
+
+    mv sixhour-X.X.X [myapp]
+    
+    cd myapp
+    
+    gem bundle
+    
+    ./bin/rackup
+    
+The web template should be up and running.
+
+## Testing
+
+It comes with rspec testing setup in the box.
+
+    spec ./spec
+    
+## Troubleshooting 
+
+[support@jackhq.com](mailto:support@jackhq.com)
+    
+    
